5ac3eb295cf3ce320985a4fc540bb557e1e20a1f
mmdinu <75750050+mmdinu@users.noreply.github.com>
2021-03-23 22:36:56 +0200

    Am adaugat Demo Java

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Here's my first Java program!");
+        System.out.println("I repeat structure from VS code!");
+
+
+        String name ="David";
+        int age = 42;
+        double score =15.9;
+        char group = 'Z';
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(score);
+        System.out.println(group);
+    }
+}
